b0c39773961ee15e617e2f7c1e9ba5689f583dd7
manhals <mn-angel@hotmail.com>
2021-06-04 11:36:43 -0700

    init2

--- backend4/settings.py
+++ backend4/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'backend'
 ]
 
 MIDDLEWARE = [
@@ -75,8 +76,11 @@ WSGI_APPLICATION = 'backend4.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'backend',
+        'USER': 'backenduser',
+        'PASSWORD': 'backend',
+        'HOST': 'localhost'
     }
 }
 
